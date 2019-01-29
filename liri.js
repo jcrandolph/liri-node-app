148c20b72cf13b14bf115cc1d6b349fe96d2623f
jcrandolph <jackcrandi@gmail.com>
2019-01-29 03:08:10 -0500

    sun2

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-//var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
 var axios = require("axios");
+var fs = require("fs");
 
 if (process.argv[2] === "movie-this") {
     var movieName = process.argv[3];
@@ -22,26 +23,28 @@ if (process.argv[2] === "movie-this") {
             console.log("Language: " + response.data.Language);
             console.log("Plot: " + response.data.Plot);
             console.log("Actors: " + response.data.Actors);
-        }
-    );
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        })
 }
 
-//else if (process.argv[2] === "spotify-this") {
-  //  var songName = process.argv[3];
-   // var spotifyUrl = "https://api.spotify.com/v1/search?query=" + songName + "\u0026offset=0\u0026limit=20\u0026type=track", "items": [{ "external_urls": { "spotify": "https://open.spotify.com/artist/08td7MxkoHQkXnWAYD8d6Q" }, "genres": [], "href": "https://api.spotify.com/v1/artists/08td7MxkoHQkXnWAYD8d6Q", "id": "08td7MxkoHQkXnWAYD8d6Q", "images": [{ "height": 640, "url": "https://i.scdn.co/image/f2798ddab0c7b76dc2d270b65c4f67ddef7f6718", "width": 640 }, { "height": 300, "url": "https://i.scdn.co/image/b414091165ea0f4172089c2fc67bb35aa37cfc55", "width": 300 }, { "height": 64, "url": "https://i.scdn.co/image/8522fc78be4bf4e83fea8e67bb742e7d3dfe21b4", "width": 64 }], "name": "Tania Bowra", "popularity": 0, "type": "artist", "uri": "spotify:artist:08td7MxkoHQkXnWAYD8d6Q" }], "limit": 20, "next": null, "offset": 0, "previous": null, "total": 1
+else if (process.argv[2] === "spotify-this") {
+    var spotify = new Spotify(keys.spotify)
+    var songName = process.argv[3];
+    if (songName = undefined) {
+        songName = "the sign";
+    }
+    spotify.search({ type: "track", query: songName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
-   // if (songName = undefined) {
-    //    songName = "the sign"
-   // }
-//    axios.get(spotifyUrl).then(
-//        function (response) {
-//            console.log("Artist(s): " + response.data.Artists);
-//            console.log("Song Name: " + response.data.Track);
-//            console.log("Preview Link: " + response.data.imdbRating);
-//            console.log("Album: " + response.data.Album);
-//        }
-//    );
-//}
+        console.log("Artist(s): " + data.tracks.artists);
+        console.log("Song name: " + data.tracks.track);
+        console.log("Preview link: " + data.tracks.preview_url);
+        console.log("Album: " + data.tracks.album)
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    })
+}
 
 
 else if (process.argv[2] === "concert-this") {
@@ -51,10 +54,17 @@ else if (process.argv[2] === "concert-this") {
 
     axios.get(bandsUrl).then(
         function (response) {
+
             console.log("Venue: " + response.data[0].venue.name);
             console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
             console.log("Date: " + response.data[0].datetime);
-        }
-    )
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        })
 }
 
+else if (process.argv[2] === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+          return console.log(err);
+        }
+}
